f68980a67a22fe06bc06e943dea2ee8c34ae32ef
majcikx <majcikx@icloud.com>
2024-03-19 22:39:28 +0100

    dalsi commit

--- src/main/java/MyCompany/StandAloneTest_152.java
+++ src/main/java/MyCompany/StandAloneTest_152.java
@@ -1,12 +1,15 @@
 package MyCompany;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.bouncycastle.jcajce.provider.drbg.DRBG;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import java.time.Duration;
 import java.util.List;
@@ -14,12 +17,14 @@ import java.util.List;
 public class StandAloneTest_152 {
 
     public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup();
+        //WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         //setup of general wait for test
+        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://rahulshettyacademy.com/client");
 
+        String productName = "ZARA COAT 3";
         //MxTest@gmail.com/Password123
         driver.findElement(By.id("userEmail")).sendKeys("MxTest@gmail.com");
         driver.findElement(By.id("userPassword")).sendKeys("Password123");
@@ -39,6 +44,24 @@ public class StandAloneTest_152 {
         //wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".ng-animating")));
         wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
         driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+
+        List<WebElement> cartProducts =  driver.findElements(By.cssSelector(".cartSection h3"));
+        Boolean match = cartProducts.stream().anyMatch(cartProduct -> cartProduct.getText().equalsIgnoreCase(productName));
+
+        Assert.assertTrue(match);
+
+        driver.findElement(By.cssSelector(".totalRow button")).click();
+
+        Actions a = new Actions(driver);
+        a.sendKeys(driver.findElement(By.cssSelector("[placeholder='Select Country']")),"india").build().perform();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ta-results")));
+        driver.findElement(By.xpath("//button[contains(@class,'ta-item')])[2]")).click();
+        driver.findElement(By.cssSelector(".action__submit")).click();
+
+        String confirmMessage = driver.findElement(By.cssSelector(".hero-primary")).getText();
+        Assert.assertTrue(confirmMessage.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
+
         driver.quit();
 
     }
